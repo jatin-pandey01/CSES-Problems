0dfa2dbe195e3ad9372103bc2f2e394d3cc88eea
jatin-pandey01 <jatinpandey3250@gmail.com>
2023-08-07 17:17:08 +0530

    Solved

---
+++ missingNumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<map>
+// #include<algorithm>
+using namespace std;
+ 
+int main(){
+	int n;
+	cin>>n;
+	map<int,bool> mapping;
+	for(int i=1;i<n;i++){
+		int a;
+		cin>>a;
+		mapping[a] = true;
+	}
+	for(int i=1;i<=n;i++){
+		if(!mapping[i]) cout<<i<<endl;
+	}
+    return 0;
+}
